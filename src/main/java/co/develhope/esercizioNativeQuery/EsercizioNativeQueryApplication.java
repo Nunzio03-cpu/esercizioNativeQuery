7bd4663550b6aa651a62f4740b99796cce4b6d1f
Nunzio <nunziodebartolomeo03@gmail.com>
2025-02-25 10:25:45 +0100

    init

---
+++ src/main/java/co/develhope/esercizioNativeQuery/EsercizioNativeQueryApplication.java
@@ -0,0 +1,13 @@
+package co.develhope.esercizioNativeQuery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioNativeQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioNativeQueryApplication.class, args);
+	}
+
+}
