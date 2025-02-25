378e7517ec86b317d8521c38d6da306c36b04a10
Nunzio <nunziodebartolomeo03@gmail.com>
2025-02-25 12:14:43 +0100

    creation endpoint GET /seleziona-ogni-prodotto

--- src/main/java/co/develhope/esercizioNativeQuery/Controller/ProdottoController.java
+++ src/main/java/co/develhope/esercizioNativeQuery/Controller/ProdottoController.java
@@ -4,10 +4,10 @@ import co.develhope.esercizioNativeQuery.Entity.Prodotto;
 import co.develhope.esercizioNativeQuery.Service.ProdottoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/prodotto")
@@ -15,8 +15,25 @@ public class ProdottoController {
     @Autowired
     private ProdottoService prodottoService;
 
+    /**
+     *  Endpoint: POST /crea-prodotto
+     *  Descrizione: crea un nuovo prodotto
+     *  @param prodotto
+     *  @return nuovo oggetto prodotto
+     */
     @PostMapping("/crea-prodotto")
     public ResponseEntity<Prodotto> creaProdotto(@RequestBody Prodotto prodotto){
         return ResponseEntity.ok(prodottoService.creaProdotto(prodotto));
+    }
+
+    /**
+     *  Endpoint: GET /seleziona-ogni-prodotto
+     *  Descrizione: seleziona tutti i prodotti creati
+     *  @return un arraylist con tutti i prodotti creati
+     */
+    @GetMapping("/seleziona-ogni-prodotto")
+    public ResponseEntity<ArrayList<Prodotto>> selezionaOgniProdotto(){
+        List<Prodotto> prodotti = prodottoService.selezionaOgniProdotto();
+        return ResponseEntity.ok(new ArrayList<>(prodotti));
     }
 }
--- src/main/java/co/develhope/esercizioNativeQuery/Service/ProdottoService.java
+++ src/main/java/co/develhope/esercizioNativeQuery/Service/ProdottoService.java
@@ -5,6 +5,9 @@ import co.develhope.esercizioNativeQuery.Repository.ProdottoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ProdottoService {
     @Autowired
@@ -12,5 +15,10 @@ public class ProdottoService {
 
     public Prodotto creaProdotto(Prodotto prodotto){
         return prodottoRepository.save(prodotto);
+    }
+
+    public ArrayList<Prodotto> selezionaOgniProdotto(){
+        List<Prodotto> prodotti = prodottoRepository.findAll();
+        return new ArrayList<>(prodotti);
     }
 }
